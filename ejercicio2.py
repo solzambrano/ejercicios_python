82a6bd67e5aa85f985aa65d1231b54d6f4847d89
solzambrano <solzambrano.8@gmail.com>
2023-08-07 20:23:48 -0300

    segundo ejercicio

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,17 @@
 #Escribir un programa que pida al usuario dos números y devuelva su división. Si el usuario no introduce números debe devolver un aviso de error y si el divisor es cero también.
+
+def division (dividendo,divisor):
+    if(divisor!=0):
+        print(dividendo/divisor)
+    else:
+        print('el divisor debe ser distnto de 0')
+
+def inputUser():
+    try:
+        dividendo=int(input('ingrese el dividendo'))
+        divisor=int(input('ingrese el divisor'))
+        division(dividendo,divisor)
+    except: print('debe ingresar numeros')
+
+inputUser()
+
